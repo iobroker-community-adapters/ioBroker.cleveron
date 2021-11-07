075b4ae6011bb257633985ac41029aba39d9389d
forelleblau <44144152+forelleblau@users.noreply.github.com>
2021-11-07 15:53:26 +0100

    001dev

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+  'cleveron adapter settings': {
+    'en': 'Adapter settings for cleveron',
+    'de': 'Adaptereinstellungen für cleveron',
+    'ru': 'Настройки адаптера для cleveron',
+    'pt': 'Configurações do adaptador para cleveron',
+    'nl': 'Adapterinstellingen voor cleveron',
+    'fr': "Paramètres d'adaptateur pour cleveron",
+    'it': "Impostazioni dell'adattatore per cleveron",
+    'es': 'Ajustes del adaptador para cleveron',
+    'pl': 'Ustawienia adaptera dla cleveron',
+    'zh-cn': 'cleveron的适配器设置'
+  },
+  'user': {
+    "en": "user",
+    "de": "Benutzer",
+    "ru": "Пользователь",
+    "pt": "do utilizador",
+    "nl": "gebruiker",
+    "fr": "utilisateur",
+    "it": "utente",
+    "es": "usuario",
+    "pl": "użytkownik",
+    "zh-cn": "用户"
+  },
+  'login': {
+    "en": "Just fill in you cleveron login data",
+    "de": "Geben Sie einfach Ihre cleveron-Login-Daten ein",
+    "ru": "Просто введите данные для входа в cleveron",
+    "pt": "Basta preencher seus dados de login inteligentes",
+    "nl": "Vul je inloggegevens slim in",
+    "fr": "Remplissez simplement vos données de connexion cleveron",
+    "it": "Basta inserire i tuoi dati di accesso intelligenti",
+    "es": "Simplemente complete sus datos de inicio de sesión de cleveron",
+    "pl": "Wystarczy wpisać sprytne dane logowania",
+    "zh-cn": "只需填写您的cleveron登录数据"
+  }
+};
